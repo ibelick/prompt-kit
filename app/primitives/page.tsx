0c5c4866bab3859cc4550f07b03ed657ea66b393
ibelick <julien.thibeaut@gmail.com>
2025-07-31 10:36:56 +0200

    feat: init primitives with chatbot

---
+++ app/primitives/page.tsx
@@ -0,0 +1,50 @@
+import FullStackPreview from "@/components/app/fullstack-preview"
+import { LayoutProse } from "@/components/app/layout-prose"
+import { Button } from "@/components/ui/button"
+import { getBaseUrl } from "@/lib/utils"
+import { generateMetadata } from "../docs/utils/metadata"
+
+export const metadata = generateMetadata(
+  "Primitives",
+  "Building blocks for AI apps. Clean, composable blocks built with shadcn/ui and prompt-kit. Use them to ship faster, works with any React framework."
+)
+
+export default function PrimitivesPage() {
+  const baseUrl = getBaseUrl()
+
+  return (
+    <div className="mb-12 flex flex-col items-start">
+      <div className="mb-10 flex flex-col gap-1 text-pretty">
+        <p className="text-primary text-3xl font-[450] tracking-tight">
+          Primitives
+        </p>
+        <p className="text-muted-foreground max-w-2xl text-lg font-normal">h</p>
+      </div>
+      <LayoutProse className="flex w-full flex-col gap-12">
+        <div>
+          <h4>Full chatbot</h4>
+          <FullStackPreview
+            url={`${baseUrl}/p/chatbot`}
+            uiFilePath="components/primitives/chatbot.tsx"
+            apiFilePath="app/api/chatbot/route.ts"
+            classNameComponentContainer="p-0 aspect-video h-[650px] w-full overflow-y-auto"
+          />
+        </div>
+      </LayoutProse>
+      <div className="border-border mt-12 flex w-full flex-col items-center justify-center gap-2 rounded-md border border-dashed p-6">
+        <p className="text-muted-foreground mb-0.5 text-sm">
+          Something missing?
+        </p>
+        <Button variant="outline" asChild size="sm">
+          <a
+            href="https://github.com/ibelick/prompt-kit/issues/new?title=%5BBlock+Request%5D+&labels=block&template=block_request.yml"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Suggest a new block
+          </a>
+        </Button>
+      </div>
+    </div>
+  )
+}
