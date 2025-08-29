74cfd46f4efed4f09a0385edbe4f66dccb65a176
ibelick <julien.thibeaut@gmail.com>
2025-08-29 18:51:57 +0200

    fix: dark mode color install code block

--- components/app/doc-code-block.tsx
+++ components/app/doc-code-block.tsx
@@ -18,7 +18,7 @@ export async function DocCodeBlock({
     <ClientCodeWrapper code={code}>
       <div
         dangerouslySetInnerHTML={{ __html: html }}
-        className="not-prose bg-background overflow-auto rounded-md border border-zinc-200 p-2 text-[13px]"
+        className="not-prose bg-background border-border overflow-auto rounded-md border p-2 text-[13px] dark:[&_pre]:!bg-transparent dark:[&_span]:!text-white"
         {...props}
       />
     </ClientCodeWrapper>
