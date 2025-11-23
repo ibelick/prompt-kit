e8977b172c40cb6ed4cd89a2b52bdb83aaf9e74f
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-23 14:05:24 -0800

    Bump Streamdown, fix CSS

--- components/prompt-kit/markdown.tsx
+++ components/prompt-kit/markdown.tsx
@@ -1,8 +1,13 @@
+"use client"
+
+import { cn } from "@/lib/utils";
 import { ComponentPropsWithoutRef } from "react"
 import { Streamdown } from "streamdown"
 
 export type MarkdownProps = ComponentPropsWithoutRef<typeof Streamdown>;
 
-export const Markdown = (props: MarkdownProps) => (
-  <Streamdown {...props} />
+export const Markdown = ({ className, ...props }: MarkdownProps) => (
+  <div className="not-prose">
+    <Streamdown className={cn("[&>*:first-child]:mt-0 [&>*:last-child]:mb-0", className)} {...props} />
+  </div>
 );
