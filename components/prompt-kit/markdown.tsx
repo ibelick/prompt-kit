5dc4fc5d1a6d216b541441311f49755be177f40c
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-23 14:46:17 -0800

    Misc styling fixes

--- components/prompt-kit/markdown.tsx
+++ components/prompt-kit/markdown.tsx
@@ -7,7 +7,7 @@ import { Streamdown } from "streamdown"
 export type MarkdownProps = ComponentPropsWithoutRef<typeof Streamdown>;
 
 export const Markdown = ({ className, ...props }: MarkdownProps) => (
-  <div className="not-prose">
+  <div className="not-prose [&_[data-streamdown=code-block]]:border-foreground/10 [&_pre]:bg-background!">
     <Streamdown className={cn("[&>*:first-child]:mt-0 [&>*:last-child]:mb-0", className)} {...props} />
   </div>
 );
